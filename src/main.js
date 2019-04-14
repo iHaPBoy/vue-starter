a6eb08d05ff15fdf19a89cf831fba460d417ac8b
HaPBoy <ihapboy@gmail.com>
2019-04-14 23:59:17 +0800

    add global css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import { VueAxios } from './utils/axios'
 
+import './styles/index.scss' // global css
 import './icons' // icons
 
 Vue.config.productionTip = false
